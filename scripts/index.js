b5763ce8b35241aff7c1ce923de1c4a130d2736e
Tatiana Smakova <tensmakova@gmail.com>
2023-01-26 21:32:49 +0500

    изменения в названиях переменных и функций

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
-const buttonEdit = document.querySelector('.profile__button-edit');
-const closeButton = document.querySelectorAll('.popup__button-close');
-const popupEdit = document.querySelector('.popup_edit');
-const formEdit = document.querySelector('.popup__form_edit');
-const nameInput = popupEdit.querySelector('.popup__input_value_name');
-const jobInput = popupEdit.querySelector('.popup__input_value_job');
+const buttonOpenEditProfile = document.querySelector('.profile__button-edit');
+const popupEditProfile = document.querySelector('.popup_edit');
+const formEditProfile = document.querySelector('.popup__form_edit');
+const nameInput = popupEditProfile.querySelector('.popup__input_value_name');
+const jobInput = popupEditProfile.querySelector('.popup__input_value_job');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
-const popupSave = document.querySelector('.popup__button-save');
+const buttonClosePopup = document.querySelectorAll('.popup__button-close');
+const buttonSave = document.querySelector('.popup__button-save');
 
-const buttonAdd = document.querySelector('.profile__button-add');
-const popupAdd = document.querySelector('.popup_add')
-const formAdd = document.querySelector('.popup__form_add');
-const inputTitle = formAdd.querySelector('.popup__input_value_title');
-const inputLink = formAdd.querySelector('.popup__input_value_link');
+const buttonOpenAddProfile = document.querySelector('.profile__button-add');
+const popupAddProfile = document.querySelector('.popup_add')
+const formAddProfile = document.querySelector('.popup__form_add');
+const titleInput = formAddProfile.querySelector('.popup__input_value_title');
+const linkInput = formAddProfile.querySelector('.popup__input_value_link');
 const feedList = document.querySelector('.feed__list');
 
 const feedTemplate = document.querySelector('.feed__template').content;
@@ -20,71 +20,44 @@ const popupImg = document.querySelector('.popup_img');
 const imageZoom = popupImg.querySelector('.popup__img-zoom');
 const titleZoom = popupImg.querySelector('.popup__img-title');
 
-
-function popupOpen (popupElement) {
+function openPopup (popupElement) {
   popupElement.classList.add('popup_opened');
 };
-function popupClose (popupElement) {
+function closePopup (popupElement) {
   popupElement.classList.remove('popup_opened');
 };
 
-closeButton.forEach((button) => {
+buttonClosePopup.forEach((button) => {
   button.addEventListener('click', () => {
-    popupClose(button.closest('.popup'))
+    closePopup(button.closest('.popup'))
   });
 });
 
-buttonEdit.addEventListener('click', () => {
-  popupOpen(popupEdit);
+buttonOpenEditProfile.addEventListener('click', () => {
+  openPopup(popupEditProfile);
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 });
-function handleFormSubmitProfile(evt) {
+function submitEditProfile(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popupClose(popupEdit);
+  closePopup(popupEditProfile);
 };
-formEdit.addEventListener('submit', handleFormSubmitProfile);
+formEditProfile.addEventListener('submit', submitEditProfile);
 
-buttonAdd.addEventListener('click', () => {
-  popupOpen(popupAdd);
+buttonOpenAddProfile.addEventListener('click', () => {
+  openPopup(popupAddProfile);
 });
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 const zoomImg = (item) => {
   item.addEventListener('click', (evt) => {
-  popupOpen(popupImg);
-  titleZoom.textContent = evt.target.closest('.feed__item').textContent;
-  imageZoom.src = item.src;
-  imageZoom.alt = item.alt;
+    openPopup(popupImg);
+    titleZoom.textContent = evt.target.closest('.feed__item').textContent;
+    imageZoom.src = item.src;
+    imageZoom.alt = item.alt;
   });
-  };
+};
 
 const newFeed = (name, link) => {
   const feedElement = feedTemplate.cloneNode(true);
@@ -112,10 +85,10 @@ initialCards.forEach((item) => {
   feedList.append(newFeed(item.name, item.link, item.alt));
 });
 
-function handleFormSubmitAdd(evt) {
+function submitAddProfile(evt) {
   evt.preventDefault();
-  feedList.prepend(newFeed(inputTitle.value, inputLink.value));
+  feedList.prepend(newFeed(titleInput.value, linkInput.value));
   evt.target.reset();
-  popupClose(popupAdd);
+  closePopup(popupAddProfile);
 };
-formAdd.addEventListener('submit', handleFormSubmitAdd);
+formAddProfile.addEventListener('submit', submitAddProfile);
